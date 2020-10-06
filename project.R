e7584c784a0a4501676b610c1f5206cceb3579bf
menchit <thomas.menchi@edu.esiee.fr>
2020-10-06 13:18:47 +0200

    new dataset

--- project.R
+++ project.R
@@ -7,10 +7,9 @@ data <- read.table("abo.csv",header = TRUE, sep = ",")
 
 colnames(data) <- c("LOCATION","Pays","VAR","Variable","TIME","Temps","Unit Code","Unit","PowerCode Code","PowerCode","Reference Period Code","Reference Period","Value","Flag Codes","Flags")
 
-select <- c("LOCATION","VAR","TIME","Unit Code","PowerCode Code","PowerCode","Reference Period Code","Value")
+select <- c("LOCATION","VAR","TIME","Unit Code","PowerCode Code","PowerCode","Value")
 data_c <- data[select]
-
-print(summary(data_c))
+data_c$VAR <- as.factor(data_c$VAR)
 
 
 write.csv(data_c, file = "data_c.csv", row.names=FALSE)

f48f9e9357c4171eafd189a2485ded8953f555e7
menchit <thomas.menchi@edu.esiee.fr>
2020-10-23 10:45:04 +0200

    carte choro

--- map.R
+++ map.R
@@ -3,13 +3,14 @@ library(geojsonio)
 library(geojsonR)
 
 
-states <- geojsonio::geojson_read("world.json", what = "sp");
-class(states);
+states <- geojsonio::geojson_read("world.json", what = "sp")
+class(states)
+
 
 m <- leaflet(states) %>%
   addTiles() %>%
   addPolygons() %>%
-  setView(-96, 37.8, 4) %>%
+  setView(40, 3, 1.8) %>%
   addProviderTiles("MapBox", options = providerTileOptions(
     id = "mapbox.light",
     accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN')))

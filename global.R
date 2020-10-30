1820bc2baaa0172aec75c09685875209ee40756d
menchit <thomas.menchi@edu.esiee.fr>
2020-10-30 18:27:51 +0100

    merge

---
+++ global.R
@@ -0,0 +1,31 @@
+files <- list.files("./data_world")
+
+world <- geojsonio::geojson_read("world.json", what = "sp")
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>Level of life satisfaction : %g ",
+  world$name, world$Value
+) %>% lapply(htmltools::HTML)
+
+df = read.csv("data_world/happiness-cantril-ladder.csv", header = TRUE, sep = ",")
+names(df) <- c("Entity", "Code", "Year", "Value")
+
+df2 = read.csv("data_world/political-regime-updated2016.csv", header = TRUE, sep = ",")
+names(df2) <- c("Entity", "Code", "Year", "NewValue")
+
+df3 = inner_join(df, df2, by = NULL, copy = FALSE)
+df3$Year <- as.factor(df3$Year)
+df3$Entity <- as.factor(df3$Entity)
+df3$Continent <- countrycode(sourcevar = df3[, "Code"],
+                             origin = "iso3c",
+                             destination = "continent")
+
+df3$Continent <- as.factor(df3$Continent)
+yeardf <- df$Year %>% unique() %>% sort()
+yeardf2 <- df2$Year %>% unique() %>% sort()
+diffyears <- yeardf[yeardf %in% yeardf2]
+
+world$Value <- df$Value[match(world$name, df$Entity)]
+bins <- c(2, 3, 4, 5, 6, 7, 8, 9)
+pal <- colorBin("GnBu", domain = df$Value, bins = bins)
+
